1741b4cc628f773d07ab1cb0ac49c5f91e96abb2
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2020-04-01 22:04:32 +0300

    Update MainFrame.java

--- company/MainFrame.java
+++ company/MainFrame.java
@@ -8,7 +8,7 @@ public class MainFrame extends JFrame
     ConfigPanel configPanel;
     ControlPanel controlPanel;
     DrawPanel canvas;
-
+    SelectShapePanel selectShapePanel;
     public MainFrame() {
         super("Better than Draw.io");
         init();
@@ -20,12 +20,11 @@ public class MainFrame extends JFrame
         canvas = new DrawPanel(this);
         configPanel = new ConfigPanel(this);
         controlPanel = new ControlPanel(this);
-
-
+        selectShapePanel = new SelectShapePanel(this);
         add(canvas, BorderLayout.CENTER);
         add(configPanel, BorderLayout.NORTH);
         add(controlPanel, BorderLayout.SOUTH);
-
+        add(selectShapePanel, BorderLayout.WEST);
         pack();
     }
 
