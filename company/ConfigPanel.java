3a0f4ee95759bd6c5b99966f5ed5c890dff543c1
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2020-04-01 21:56:47 +0300

    Update ConfigPanel.java

--- company/ConfigPanel.java
+++ company/ConfigPanel.java
@@ -12,8 +12,15 @@ public class ConfigPanel extends JPanel
     JLabel label1;
     JLabel label2;
     JLabel label3;
+    JLabel labelc1;
+    JLabel labelc2;
+    JLabel labelS1;
+    JLabel labelS2;
+    JLabel labelS3;
     JSpinner sidesField;
+    JSpinner nrOfPointsField;
     JSpinner sizeField;
+    JSpinner radiusField;
     JComboBox colorCombo;
     JCheckBox randSizeCheck;
 
@@ -23,6 +30,7 @@ public class ConfigPanel extends JPanel
     }
     private void init()
     {
+        // Regular Polygon
         label1 = new JLabel("Number of sides:");
         label2 = new JLabel("Color:");
         label3 = new JLabel("Size:");
@@ -39,6 +47,18 @@ public class ConfigPanel extends JPanel
         add(label2);
         add(colorCombo);
         add(label3);
+        add(sizeField);
         add(randSizeCheck);
+
+        //  Circle
+        labelc1= new JLabel("Color: ");
+        labelc2= new JLabel("Radius: ");
+        radiusField = new JSpinner(new SpinnerNumberModel(10, 10, 125, 5));
+
+        // Star
+        labelS1= new JLabel("Color: ");
+        labelS2= new JLabel("Radius: ");
+        labelS3= new JLabel("Number of points: ");
+        nrOfPointsField= new JSpinner(new SpinnerNumberModel(5, 5, 10, 1));
     }
 }
